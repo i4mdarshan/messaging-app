146ce0d92ed2c599bfab8587d5dbdf53d0be040c
Darshan <darshan.mahajan77777@gmail.com>
2025-04-19 19:04:32 +0530

    Set redirect to chat page and remove unwanted pages

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/chat', function () { return Inertia::render('Chat');})->name('chat');
-    Route::get('/dashboard', function () { return Inertia::render('Dashboard');})->name('dashboard');
 
 });
 
